2efd12ea6535ca24b7ac4a21a35664f5cb8a5188
nazomeku <cols.nazo@gmail.com>
2017-12-20 15:14:07 +0100

    initialize tables

--- app/__init__.py
+++ app/__init__.py
@@ -24,3 +24,27 @@ Bootstrap(app)
 
 # Load the views and models
 from app import views, models
+
+
+# Transports table init.
+def db_transports_init():
+    all_transports = models.Transport.query.all()
+    if len(all_transports) < 10:
+        for _ in range(1, 11):
+            transport = models.Transport(product_name=None, product_id=None)
+            db.session.add(transport)
+        db.session.commit()
+
+
+# Shelfs table init.
+def db_shelf_init():
+    all_shelfs = models.Shelf.query.all()
+    if len(all_shelfs) < 100:
+        for _ in range(1, 101):
+            shelf = models.Shelf(product_id=None)
+            db.session.add(shelf)
+        db.session.commit()
+
+
+db_transports_init()
+db_shelf_init()
