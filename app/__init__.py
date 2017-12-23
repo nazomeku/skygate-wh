41761a47f34c505ce0048ae867cd74265a764570
nazomeku <cols.nazo@gmail.com>
2017-12-23 16:20:37 +0100

    update init

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-# Imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
@@ -6,10 +5,15 @@ from flask_bootstrap import Bootstrap
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
+Bootstrap(app)
 
 
 # Load the config
 app.config.from_object('config')
+
+
+# Define the database object
+db = SQLAlchemy(app)
 
 
 # Fix jinja spacing
@@ -17,34 +21,13 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 
-# Define the database object
-db = SQLAlchemy(app)
-Bootstrap(app)
+# Import views and db_init.
+from . import views, db_init
 
-
-# Load the views and models
-from app import views, models
-
-
-# Transports table init.
-def db_transports_init():
-    all_transports = models.Transport.query.all()
-    if len(all_transports) < 10:
-        for _ in range(1, 11):
-            transport = models.Transport(product_name=None, product_id=None)
-            db.session.add(transport)
-        db.session.commit()
-
-
-# Shelfs table init.
-def db_shelf_init():
-    all_shelfs = models.Shelf.query.all()
-    if len(all_shelfs) < 100:
-        for _ in range(1, 101):
-            shelf = models.Shelf(product_id=None)
-            db.session.add(shelf)
-        db.session.commit()
-
-
-db_transports_init()
-db_shelf_init()
+# Initialize databases.
+@app.before_first_request
+def setup():
+    db_init.db_products_init()
+    db_init.db_transports_init()
+    db_init.db_shelf_init()
+    #views.random_fill()
